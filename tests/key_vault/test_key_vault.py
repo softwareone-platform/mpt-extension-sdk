b8f7f4a20eba462f785b4f0f73aa8982d9a46c00
Robert Segal <robert.segal@softwareone.com>
2025-04-07 09:12:49 -0600

    Added get and set azure key vault secret

---
+++ tests/key_vault/test_key_vault.py
@@ -0,0 +1,59 @@
+from azure.keyvault.secrets import SecretClient
+
+from mpt_extension_sdk.key_vault.base import KeyVault
+
+
+def test_init_key_vault(
+    mock_key_vault_name
+):
+    key_vault = KeyVault(mock_key_vault_name)
+    assert key_vault.key_vault_name == mock_key_vault_name
+
+
+def test_get_secret(
+    mocker,
+    mock_key_vault_name,
+    mock_secret_name,
+    mock_key_vault_url
+):
+    key_vault = KeyVault(mock_key_vault_name)
+    mock_secret_value = mocker.MagicMock()
+    mock_secret_value.value = "mock_secret_value"
+    mock_client = mocker.MagicMock()
+    mocker.patch.object(
+        key_vault, "_get_key_vault_client", return_value = mock_client
+    )
+    mocker.patch.object(
+        key_vault, "_get_key_vault_url", return_value = mock_key_vault_url
+    )
+    mocker.patch.object(
+        mock_client, "get_secret", return_value = mock_secret_value
+    )
+    secret = key_vault.get_secret(mock_secret_name)
+    assert secret == mock_secret_value.value
+
+
+def test_set_secret(
+    mocker,
+    mock_key_vault_name,
+    mock_secret_name,
+    mock_key_vault_url
+):
+    key_vault = KeyVault(mock_key_vault_name)
+    mock_secret_value = mocker.MagicMock()
+    mock_secret_value.value = "mock_secret_value"
+    mock_client = mocker.MagicMock()
+    mocker.patch.object(
+        key_vault, "_get_key_vault_client", return_value = mock_client
+    )
+    mocker.patch.object(
+        key_vault, "_get_key_vault_url", return_value = mock_key_vault_url
+    )
+    mocker.patch.object(
+        mock_client, "set_secret", return_value = mock_secret_value.value
+    )
+    mocker.patch.object(
+        mock_client, "get_secret", return_value = mock_secret_value
+    )
+    secret = key_vault.get_secret(mock_secret_name)
+    assert secret == mock_secret_value.value
