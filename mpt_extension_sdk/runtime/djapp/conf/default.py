87509f48b0c81ad20a5ad9ba1e07ab31237891d2
Robert Segal <robert.segal@softwareone.com>
2025-05-06 09:41:10 -0600

    Temporarily excluding coverage, removing a django key

--- mpt_extension_sdk/runtime/djapp/conf/default.py
+++ mpt_extension_sdk/runtime/djapp/conf/default.py
@@ -25,12 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(
-    "MPT_DJANGO_SECRET_KEY",
-    "django-insecure-6r_%9ku+bg0=@xw1ah$wh+liwbsyhwpn#6alt*ppjn8t_uyp-u",
-)
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
