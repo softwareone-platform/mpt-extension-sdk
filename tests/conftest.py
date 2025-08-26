4f2c4bf56cfd5a823444fb961a9c09dd40f6f2de
Susana <3016283+svazquezco@users.noreply.github.com>
2025-08-26 10:46:26 +0200

    MPT-12383: add create and update assets methods (#61)
    Add methods to create and update assets in MPT

--- tests/conftest.py
+++ tests/conftest.py
@@ -141,8 +141,9 @@ def runtime_master_options_factory(
 
 @pytest.fixture()
 def mock_highlights(mock_logging_all_prefixes):
-    return _ReprHighlighter.highlights + [
-        rf"(?P<mpt_id>(?:{'|'.join(mock_logging_all_prefixes)})(?:-\d{{4}})*)"
+    return [
+        *_ReprHighlighter.highlights,
+        rf"(?P<mpt_id>(?:{'|'.join(mock_logging_all_prefixes)})(?:-\d{{4}})*)",
     ]
 
 
@@ -729,7 +730,30 @@ def webhook(settings):
     }
 
 
-@pytest.fixture()
+@pytest.fixture
+def assets_factory(lines_factory):
+    def _assets(
+        asset_id="AST-1000-2000-3000",
+        product_name="Awesome product",
+        vendor_subscription_id="a-sub-id",
+        lines=None,
+    ):
+        lines = lines_factory() if lines is None else lines
+        return [
+            {
+                "id": asset_id,
+                "name": f"Subscription for {product_name}",
+                "lines": lines,
+                "externalIds": {"vendor": vendor_subscription_id},
+                "status": "Active",
+                "price": {"PPx1": 3148.80000, "currency": "USD"},
+            }
+        ]
+
+    return _assets
+
+
+@pytest.fixture
 def subscriptions_factory(lines_factory):
     def _subscriptions(
         subscription_id="SUB-1000-2000-3000",
@@ -739,9 +763,7 @@ def subscriptions_factory(lines_factory):
         commitment_date=None,
         lines=None,
     ):
-        start_date = (
-            start_date.isoformat() if start_date else datetime.now(UTC).isoformat()
-        )
+        start_date = start_date.isoformat() if start_date else dt.datetime.now(dt.UTC).isoformat()
         lines = lines_factory() if lines is None else lines
         return [
             {
