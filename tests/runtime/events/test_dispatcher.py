7bf293c21e61f8760ac890e7c313a33e0c99f85f
Susana Vázquez <3016283+svazquezco@users.noreply.github.com>
2026-02-05 14:47:26 +0100

    refactor: improve slowest test

--- tests/runtime/events/test_dispatcher.py
+++ tests/runtime/events/test_dispatcher.py
@@ -2,38 +2,50 @@ import threading
 from collections import deque
 from concurrent.futures import ThreadPoolExecutor
 
+import pytest
+
 from mpt_extension_sdk.core.events.dataclasses import Event
 from mpt_extension_sdk.mpt_http.base import MPTClient
 from mpt_extension_sdk.runtime.events.dispatcher import Dispatcher
 
 
+@pytest.fixture(autouse=True)
+def mock_time_sleep(mocker):
+    mocker.patch("mpt_extension_sdk.runtime.events.dispatcher.time.sleep", autospec=True)
+
+
 def test_dispatcher(mock_app_group_name):
-    dispatcher = Dispatcher(group=mock_app_group_name)
-    assert dispatcher is not None
-    assert isinstance(dispatcher.client, MPTClient)
-    assert isinstance(dispatcher.executor, ThreadPoolExecutor)
-    assert isinstance(dispatcher.queue, deque)
-    assert dispatcher.futures == {}
-    assert isinstance(dispatcher.running_event, threading.Event)
-    assert isinstance(dispatcher.processor, threading.Thread)
+    result = Dispatcher(group=mock_app_group_name)
+
+    assert result is not None
+    assert isinstance(result.client, MPTClient)
+    assert isinstance(result.executor, ThreadPoolExecutor)
+    assert isinstance(result.queue, deque)
+    assert result.futures == {}
+    assert isinstance(result.running_event, threading.Event)
+    assert isinstance(result.processor, threading.Thread)
 
 
 def test_dispatcher_running(mock_app_group_name):
     dispatcher = Dispatcher(group=mock_app_group_name)
     dispatcher.start()
-    is_running = dispatcher.running
+
+    result = dispatcher.running
+
+    assert result is True
     dispatcher.stop()
     dispatcher.executor.shutdown()
-    assert is_running
 
 
 def test_dispatcher_stop(mock_app_group_name):
     dispatcher = Dispatcher(group=mock_app_group_name)
     dispatcher.start()
     dispatcher.stop()
-    is_running = dispatcher.running
+
+    result = dispatcher.running
+
+    assert result is False
     dispatcher.executor.shutdown()
-    assert not is_running
 
 
 def test_dispatcher_dispatch_event(mock_app_group_name):
